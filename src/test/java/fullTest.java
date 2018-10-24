bdcf184cc3334aefe23f69efc6ccb921a4a66259
luastan <saulbarral@gmail.com>
2018-10-24 15:47:27 +0200

    unit tests

--- src/test/java/fullTest.java
+++ src/test/java/fullTest.java
@@ -1,10 +1,12 @@
 import ejemploTest.ejemploTestPrimer;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
+import testClases.clasesPrimer;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        ejemploTestPrimer.class
+        ejemploTestPrimer.class,
+        clasesPrimer.class
 })
 public class fullTest {
 }
---
+++ src/test/java/testClases/colocacionTest/calles/CasillaTest.java
@@ -0,0 +1,26 @@
+package testClases.colocacionTest.calles;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CasillaTest {
+
+    @BeforeClass
+    public static void preSetUp() {
+        System.out.println("Se hace antes de empezar los tests de clase1Test");
+    }
+
+
+    @Before
+    public void setUp(){
+        System.out.println("Se hace antes de cada uno de los tests de clase1Test");
+    }
+
+
+    @Test
+    public void ejemploTest1() {
+        System.out.println("Caracola");
+    }
+
+}
