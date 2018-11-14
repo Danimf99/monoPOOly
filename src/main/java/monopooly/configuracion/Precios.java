780e6d1b317bbff08cafd6d34b9e9dd8f3e3531a
luastan <saulbarral@gmail.com>
2018-11-14 21:29:53 +0100

    esto non fai falta tampouco
    Signed-off-by: luastan <saulbarral@gmail.com>

--- src/main/java/monopooly/configuracion/Precios.java
+++ src/main/java/monopooly/configuracion/Precios.java
@@ -20,16 +20,6 @@ public class Precios {
     public static final int PRECIO_VERDE=(int)(1.3*PRECIO_AMARILLO);
     public static final int PRECIO_AZUL_MARINO=(int)((1.3*PRECIO_VERDE*3)/2);
 
-    //ALQUILERES INICIALES
-    public static final int ALQUILER_MARRON=(int)(0.1*PRECIO_MARRON);
-    public static final int ALQUILER_AZUL_CLARO=(int)(0.1*PRECIO_AZUL_CLARO);
-    public static final int ALQUILER_VIOLETA=(int)(0.1*PRECIO_VIOLETA);
-    public static final int ALQUILER_NARANJA=(int)(0.1*PRECIO_NARANJA);
-    public static final int ALQUILER_ROJO=(int)(0.1*PRECIO_ROJO);
-    public static final int ALQUILER_AMARILLO=(int)(0.1*PRECIO_AMARILLO);
-    public static final int ALQUILER_VERDE=(int)(0.1*PRECIO_VERDE);
-    public static final int ALQUILER_AZUL_MARINO=(int)(0.1*PRECIO_AZUL_MARINO);
-
     // MULTIPLICADOR VALOR_EDIFICACIONES
     public static final double VALOR_CASA = 0.6;
     public static final double VALOR_HOTEL = 0.6;
