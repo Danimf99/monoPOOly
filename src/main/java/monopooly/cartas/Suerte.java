6ef6e1bb2f2de1c136bb8c168c5305b5acf2c9f6
luastan <saulbarral@gmail.com>
2018-11-28 13:11:30 +0100

    Comienzo cartas aleatorias
    Signed-off-by: luastan <saulbarral@gmail.com>

---
+++ src/main/java/monopooly/cartas/Suerte.java
@@ -0,0 +1,164 @@
+package monopooly.cartas;
+
+import monopooly.colocacion.calles.Edificaciones;
+import monopooly.colocacion.calles.Inmueble;
+import monopooly.configuracion.Carta;
+import monopooly.entradaSalida.Prompt;
+import monopooly.player.Jugador;
+
+public class Suerte {
+    public enum NumeracionCartas{
+        carta1,
+        carta2,
+        carta3,
+        carta4,
+        carta5,
+        carta6,
+        carta7,
+        carta8,
+        carta9,
+        carta10,
+        carta11,
+        carta12,
+        carta13,
+        carta14
+    }
+
+    private static final String MENSAJE_PROMPT = "Carta de Suerte";
+
+    private String mensaje;
+    private Suerte.NumeracionCartas numero;
+
+    public Suerte(String mensaje, Suerte.NumeracionCartas numero) {
+        this.mensaje = mensaje;
+        this.numero = numero;
+    }
+
+
+    public void ejecutarCarta(Prompt prompt) {
+        switch (numero) {
+            case carta1:
+                this.accionCarta1(prompt);
+                break;
+            case carta2:
+                this.accionCarta2(prompt);
+                break;
+            case carta3:
+                this.accionCarta3(prompt);
+                break;
+            case carta4:
+                this.accionCarta4(prompt);
+                break;
+            case carta5:
+                this.accionCarta5(prompt);
+                break;
+            case carta6:
+                this.accionCarta6(prompt);
+                break;
+            case carta7:
+                this.accionCarta7(prompt);
+                break;
+            case carta8:
+                this.accionCarta8(prompt);
+                break;
+            case carta9:
+                this.accionCarta9(prompt);
+                break;
+            case carta10:
+                this.accionCarta10(prompt);
+                break;
+            case carta11:
+                this.accionCarta11(prompt);
+                break;
+            case carta12:
+                this.accionCarta12(prompt);
+                break;
+            case carta13:
+                this.accionCarta13(prompt);
+                break;
+            case carta14:
+                this.accionCarta14(prompt);
+                break;
+        }
+    }
+
+
+    private void accionCarta1(Prompt prompt) {
+
+    }
+
+    private void accionCarta2(Prompt prompt) {
+
+    }
+
+    private void accionCarta3(Prompt prompt) {
+
+    }
+
+    private void accionCarta4(Prompt prompt) {
+
+    }
+
+    private void accionCarta5(Prompt prompt) {
+
+    }
+
+    private void accionCarta6(Prompt prompt) {
+
+    }
+
+    private void accionCarta7(Prompt prompt) {
+
+    }
+
+
+    private void accionCarta8(Prompt prompt) {
+        Jugador actual = prompt.getJugador();
+        int modDinero = 0;
+        for (Inmueble propiedad : actual.getPropiedades()) {
+            for (Edificaciones edificio : propiedad.getEdificios()) {
+                switch (edificio.getTipo()) {
+                    case casa:
+                        modDinero -= Carta.Suerte.Carta8.DINERO_CASA;
+                        break;
+                    case hotel:
+                        modDinero -= Carta.Suerte.Carta8.DINERO_HOTEL;
+                        break;
+                    case deporte:
+                        modDinero -= Carta.Suerte.Carta8.DINERO_PISCINA;
+                        break;
+                    case piscina:
+                        modDinero -= Carta.Suerte.Carta8.DINERO_PISTA_DEPORTE;
+                        break;
+                }
+            }
+        }
+        actual.quitarDinero(-modDinero);
+        prompt.setModificacionPasta(modDinero, MENSAJE_PROMPT);
+    }
+
+    private void accionCarta9(Prompt prompt) {
+
+    }
+
+    private void accionCarta10(Prompt prompt) {
+
+    }
+
+    private void accionCarta11(Prompt prompt) {
+
+    }
+
+    private void accionCarta12(Prompt prompt) {
+
+    }
+
+    private void accionCarta13(Prompt prompt) {
+
+    }
+
+    private void accionCarta14(Prompt prompt) {
+
+    }
+
+}
