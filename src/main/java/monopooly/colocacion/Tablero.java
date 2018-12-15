76d95b62b5658bc6903982c3a5889f6f8299e1a0
luastan <saulbarral@gmail.com>
2018-12-15 20:56:05 +0100

    Esqueleto de la clase tablero usando singleton

--- src/main/java/monopooly/colocacion/Tablero.java
+++ src/main/java/monopooly/colocacion/Tablero.java
@@ -1,5 +1,36 @@
 package monopooly.colocacion;
 
+/**
+ * Esta clase permite devolver el tablero para la partida actual.
+ *
+ * <p>La instanciación del tablero se hace al arrancar el programa. Mas info en:
+ * https://es.wikipedia.org/wiki/Singleton
+ *
+ * @author luastan
+ * @author Danimf99
+ */
 public class Tablero {
+  private static Tablero instanciaTablero;
 
+  private Tablero() {
+
+    /* Constructor de tablero */
+
+
+
+
+  }
+
+  // Inicializacion estatica de una instancia del tablero
+  static {
+    try {
+      instanciaTablero = new Tablero();
+    } catch (Exception e) {
+      throw new RuntimeException("Error inicializando el tablero");
+    }
+  }
+
+  public static Tablero getInstanciaTablero() {
+    return instanciaTablero;
+  }
 }
