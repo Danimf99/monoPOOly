97f2ff5bf833e14682066d20c069a247f8fdd975
Danimf99 <36856002+Danimf99@users.noreply.github.com>
2018-12-23 16:17:39 +0100

    Avance Describir

--- src/main/java/monopooly/entradaSalida/parsers/Describir.java
+++ src/main/java/monopooly/entradaSalida/parsers/Describir.java
@@ -2,6 +2,7 @@ package monopooly.entradaSalida.parsers;
 
 import monopooly.colocacion.Tablero;
 import monopooly.entradaSalida.Juego;
+import monopooly.player.Avatar;
 import monopooly.player.Jugador;
 
 public class Describir implements Expresion {
@@ -17,10 +18,34 @@ public class Describir implements Expresion {
     public void interpretar(Juego interprete) {
         switch(comandoIntroducido[1].toLowerCase()){
             case "jugador":
+                if(comandoIntroducido.length!=3){
+                    Juego.consola.error("Error en el comando.");
+                    Tablero.getPrompt().setHelp(true);
+                    return;
+                }
                 Jugador jugador= Tablero.getTablero().getJugador(comandoIntroducido[2]);
+                if(jugador==null){
+                    Juego.consola.info("El jugador "+comandoIntroducido[2]+" no existe.");
+                    return;
+                }
                 interprete.describirJugador(jugador);
                 break;
             case"avatar":
+                if(comandoIntroducido.length!=3){
+                    Juego.consola.error("Error en el comando.");
+                    Tablero.getPrompt().setHelp(true);
+                    return;
+                }
+                Avatar avatar=null;
+                for(Jugador jugador2:Tablero.getTablero().getJugadores()){
+                    if(jugador2.getAvatar().getRepresentacion()==comandoIntroducido[2].charAt(0)){
+                        avatar=jugador2.getAvatar();
+                    }
+                }
+                if(avatar==null){
+                    Juego.consola.info("No hay ningún avatar con esa representación");
+                    return;
+                }
                 break;
             default:
                 break;
